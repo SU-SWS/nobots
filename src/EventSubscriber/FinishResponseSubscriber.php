03ad0522af2a7f083b74ddf0d474ae1267e2c471
Mike Decker <pookmish@gmail.com>
2018-07-26 15:55:25 -0700

    Cleaned up event subscriber

--- src/EventSubscriber/FinishResponseSubscriber.php
+++ src/EventSubscriber/FinishResponseSubscriber.php
@@ -1,18 +1,23 @@
 <?php
-/**
- * Event Subscriber.
- *
- */
-
-use Drupal\Core\EventSubscriber\FinishResponseSubscriber;
-use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 
 namespace Drupal\nobots\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
  * Response subscriber to handle finished responses.
  */
-class FinishResponseSubscriber extends \Drupal\Core\EventSubscriber\FinishResponseSubscriber {
+class FinishResponseSubscriber  implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[KernelEvents::RESPONSE][] = ['onRespond'];
+    return $events;
+  }
 
   /**
    * Sets extra headers on successful responses.
@@ -20,7 +25,7 @@ class FinishResponseSubscriber extends \Drupal\Core\EventSubscriber\FinishRespon
    * @param \Symfony\Component\HttpKernel\Event\FilterResponseEvent $event
    *   The event to process.
    */
-  public function onRespond(\Symfony\Component\HttpKernel\Event\FilterResponseEvent $event) {
+  public function onRespond(FilterResponseEvent $event) {
     $request = $event->getRequest();
     $response = $event->getResponse();
     $response->headers->set('X-Robots-Tag', 'noindex,nofollow,noarchive', FALSE);
